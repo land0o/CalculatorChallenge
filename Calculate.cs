2173223264f28b911826625f8bffca65eea0b2a4
curtis <crutchc@yahoo.com>
2019-10-09 20:54:32 -0500

    part one is set up

---
+++ Calculate.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace CalculatorChallenge
+{
+    public class Calculate
+    {
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+        public int SubtractNumbers(int a, int b)
+        {
+            return a - b;
+        }
+        public int MultiplyNumbers(int a, int b)
+        {
+            return a * b;
+        }
+        public int DivideNumbers(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CalculatorChallenge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // display user menu
+            Console.WriteLine("**Welcome to Calculatron 3000!**");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Select a number from the options below...");
+            Console.WriteLine("1) Add two numbers");
+            Console.WriteLine("2) Subtract two numbers");
+            Console.WriteLine("3) Multiply two numbers");
+            Console.WriteLine("4) Divide two numbers");
+            Console.WriteLine("0) Exit");
+            Console.WriteLine();
+            Console.Write("> ");
+            Console.ReadLine();
+
+            //handle user input
+            int UserSelection = int.Parse(Console.ReadLine());
+            int MathSelection = UserSelection;
+            int a = 0;
+            int b = 0;
+            Console.Write("Please enter the first number:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Please enter the second number:");
+            b = int.Parse(Console.ReadLine());
+            int addResults = Calculate.AddNumbers(a, b);
+            Console.WriteLine($"the results of adding {a} + {b} are {addResults}");
+
+            if (MathSelection == 0)
+            {
+
+
+
+            }
+            else if (MathSelection == 1)
+            {
+
+            }
+            else if (MathSelection == 2)
+            {
+
+            }
+            else if (MathSelection == 3)
+            {
+
+            }
+            else if (MathSelection == 4)
+            {
+
+            }
+
+        }
+    }
+}
